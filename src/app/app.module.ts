5139e849d694e79078e9b72deeb1faa9ced437ae
steph-codes <ogundele370@gmail.com>
2022-12-31 02:00:26 +0100

    Managed Login Activity /Sessions with Local Storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import {CreateAccountComponent } from './pages/create-account/create-account.com
 import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http'
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { PostsComponent } from './pages/posts/posts.component';
+import {PostsComponent} from './pages/posts/posts.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 
