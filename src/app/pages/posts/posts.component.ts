3d689c585b83e0f08927f23eae8aac38497fb63b
steph-codes <ogundele370@gmail.com>
2023-01-04 22:56:20 +0100

    Create / Save a Post

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -2,7 +2,11 @@ import { Component , OnInit} from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/Services/user.service';
+import { PostService } from 'src/app/Services/post.service';
 import {finalize} from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -10,7 +14,7 @@ import {finalize} from 'rxjs/operators';
 })
 export class PostsComponent implements OnInit{
 
-    constructor(public userService: UserService, private router:Router, private storage:AngularFireStorage){
+    constructor(public userService: UserService, private router:Router, private storage:AngularFireStorage, public postService:PostService,private snackbar:MatSnackBar){
 
     }
     //upon initialization of component/ constructor run the below, if the user property in services is null means user hasnt logged in
@@ -25,9 +29,20 @@ export class PostsComponent implements OnInit{
         this.router.navigate(['/login']);
       }
     }
+
+    this.postService.getPosts().then((res:any)=>{
+      this.posts = res;
+      for(let post of this.posts){
+        this.commentText.push("");
+      }
+    }).catch((err)=>{
+      console.log(err);
+    })
   }
 
   selectedFile:any;
+
+  text= "";
 
   posts:Array<any> = [];
   commentText:Array<string> = [];
@@ -36,58 +51,51 @@ export class PostsComponent implements OnInit{
     this.selectedFile = event.target.files[0];
   }
 
+
   post(){
+    this.snackbar.open('Creating the post...', '', {duration:15000});
     if(this.selectedFile != undefined || this.selectedFile != null){
       this.uploadImage().then((imageURL)=>{
         console.log(imageURL);
+        //imageUrl created then create the postObj and push into array
+        let postObj = {
+          username: this.userService.user.username,
+          text : this.text,
+          imageURL: imageURL,
+          likes: [],
+          comments:[]
+        };
+        this.posts.push(postObj);
+        this.postService.saveNewPost(postObj).then((res)=>{
+          console.log(res);
+          this.snackbar.open('Posted successfully', 'ok');
+        }).catch((err)=>{
+          console.log(err);
+        });
+        this.selectedFile = undefined;
+
       }).catch((err)=>{
         console.log(err);
       })
     }
+    else{
+      let postObj = {
+        username: this.userService.user.username,
+        text : this.text,
+        imageURL: '',
+        likes: [],
+        comments:[]
+      };
+      //push postObj intonthe array variable
+      this.posts.push(postObj);
+      this.postService.saveNewPost(postObj).then((res)=>{
+        console.log(res);
+        this.snackbar.open('Posted successfully', 'ok');
+      }).catch((err)=>{
+        console.log(err);
+      });
+    }
   }
-
-  // post(){
-  //   this.snackbar.open('Creating the post...', '', {duration:15000});
-  //   if(this.selectedFile != undefined || this.selectedFile != null){
-  //     this.uploadImage().then((imageURL)=>{
-  //       console.log(imageURL);
-  //       let postObj = {
-  //         username: this.userService.user.username,
-  //         text : this.text,
-  //         imageURL: imageURL,
-  //         likes: [],
-  //         comments:[]
-  //       };
-  //       this.posts.push(postObj);
-  //       this.postService.saveNewPost(postObj).then((res)=>{
-  //         console.log(res);
-  //         this.snackbar.open('Posted successfully', 'ok');
-  //       }).catch((err)=>{
-  //         console.log(err);
-  //       });
-  //       this.selectedFile = undefined;
-
-  //     }).catch((err)=>{
-  //       console.log(err);
-  //     })
-  //   }
-  //   else{
-  //     let postObj = {
-  //       username: this.userService.user.username,
-  //       text : this.text,
-  //       imageURL: '',
-  //       likes: [],
-  //       comments:[]
-  //     };
-  //     this.posts.push(postObj);
-  //     this.postService.saveNewPost(postObj).then((res)=>{
-  //       console.log(res);
-  //       this.snackbar.open('Posted successfully', 'ok');
-  //     }).catch((err)=>{
-  //       console.log(err);
-  //     });
-  //   }
-  // }
 
 
   uploadImage() {
